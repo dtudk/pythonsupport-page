0ef9766ffa185b354970b678c398937590e36899
Tiago <ercolealfageme@hotmail.com>
2023-09-26 23:40:37 +0200

    parent f01f3a9fdd8ec7d52013e102c0d5833488350032 author Tiago <ercolealfageme@hotmail.com> 1695764437 +0200 committer Nick Papior <nicpa@dtu.dk> 1701173556 +0100
    added terminal documentation
    
    Author:    Tiago <ercolealfageme@hotmail.com>
    Co-authored-by: char056a <char056a@users.noreply.github.com>
    Co-authored-by: manufer20 <manufer20@users.noreply.github.com>
    Co-authored-by: zerothi <zerothi@users.noreply.github.com>
    
    Revert "Merge branch 'main' of https://github.com/TheWolf534/pythonsupport-page"
    
    This reverts commit 1bc76b148d97b4411c60a9a124339d46d00e4837, reversing
    changes made to c26d7acb2fbcd49ce59cec18757ccb9484f54f63.
    
    fixed some shit
    
    Applied feedback
    
    Næsten færdig
    
    Casi
    
    added the link
    
    corrected some windows commands
    
    further fixes to windows comand on Exercise file
    
    PS animations
    
    rendered the gifs and added them to the src code
    
    rendered the gifs and added them to the src code
    
    PS, Mac/Linx GIFs compleated and incorporated
    
    Clean up of the OS directory
    
    Finished and incorporated the CMD animations
    
    Got rid of anything related to termynal
    
    forgot 1 thing :')
    
    Corrected stuff mentioned in the comments
    
    updated lots of terminalizer stuff
    
    Signed-off-by: Nick Papior <nicpa@dtu.dk>
    
    last clean-ups
    
    Signed-off-by: Nick Papior <nicpa@dtu.dk>

---
+++ docs/os/Animations/merge.py
@@ -0,0 +1,85 @@
+import hashlib
+import sys
+from pathlib import Path
+
+import yaml
+try:
+    from yaml import CLoader as Loader, CDumper as Dumper
+except ImportError:
+    from yaml import Loader, Dumper
+
+cwd = Path()
+
+hash_file = cwd / "hashes.yml"
+tmpdir = cwd / "tmp"
+commands_out = sys.argv[1]
+
+def read_yaml(file: Path):
+    """Read a yaml file, and return the dictionary"""
+    return yaml.load(open(file, 'r'), Loader=Loader)
+
+
+def write_yaml(content: dict, file: Path):
+    """Write a dictionary to a yaml file"""
+    with open(file, 'w') as fh:
+        yaml.dump(content, fh, Dumper=Dumper)
+
+def sortdict(d):
+    return dict(sorted(d.items(), key=lambda item: item[0]))
+
+def merge(d1, d2):
+    d = {**d1}
+    for key, value in d2.items():
+        if isinstance(value, dict):
+            value = merge(d.get(key, {}), value)
+        d[key] = value
+    return sortdict(d)
+
+# Populate the hash table
+hashes = {}
+if hash_file.is_file():
+    hashes = read_yaml(hash_file)
+
+# Read the default configuration
+config = read_yaml(cwd / "config.yml")
+commands = open(commands_out, 'w')
+
+for path in cwd.glob("*/*.yml"):
+    if not path.is_file():
+        continue
+    if path.is_relative_to(tmpdir):
+        continue
+    if path.name == "config.yml":
+        continue
+
+    dir_config = read_yaml(path.parent / "config.yml")
+
+    # get a path object relative to cwd (strips segments behind)
+    top_path = path.relative_to(cwd)
+    top_path_str = str(top_path)
+
+    # Merge dictionaries
+    yml = merge(merge(config, dir_config),
+                read_yaml(path))
+
+    # check whether we need to do anything
+    content = yaml.dump(yml, Dumper=Dumper)
+    current_hash = hashlib.md5(content.encode("utf-8")).hexdigest()
+    old_hash = hashes.get(top_path_str, current_hash + "never")
+
+    # Correct the output files
+    stem = path.stem
+    out = tmpdir / '_'.join(top_path.parts)
+    gif = path.with_suffix(".gif")
+    if old_hash == current_hash and gif.is_file():
+        continue
+
+    hashes[top_path_str] = current_hash
+    # We do not have the same hash, or the file does not exist
+    write_yaml(yml, out)
+    commands.write(f"terminalizer render -o {gif} {out}\n")
+
+commands.close()
+
+# Store the hashes, to be re-used
+write_yaml(hashes, hash_file)
