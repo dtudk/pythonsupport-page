17ae3bbae13252d9446cf9e1dd4f3b9450241687
Nick Papior <nicpa@dtu.dk>
2025-05-12 12:21:04 +0200

    added state + eq to custom classes
    Signed-off-by: Nick Papior <nicpa@dtu.dk>

--- docs/conf.py
+++ docs/conf.py
@@ -102,6 +102,9 @@ except ImportError:
 
 intersphinx_mapping = {
     "python": ("https://docs.python.org/3", None),
+    "numpy": ("https://numpy.org/doc/stable/", None),
+    "scipy": ("https://docs.scipy.org/doc/scipy/", None),
+    "matplotlib": ("https://matplotlib.org/stable/", None),
 }
 
 sphinxemoji_style = "twemoji"
